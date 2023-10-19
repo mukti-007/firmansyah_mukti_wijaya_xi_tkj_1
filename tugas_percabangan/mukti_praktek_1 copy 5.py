d5317553488d7b6119624549e99ebac50bb52f58
Firmansyah Mukti Wijaya <me@ikimukti.com>
2023-10-19 13:26:09 +0700

    Mengumpukan tugas percabangan

---
+++ tugas_percabangan/mukti_praktek_1 copy 5.py
@@ -0,0 +1,21 @@
+nama :
+kelas :
+nomor
+soa1 :
+John Doe
+absen: 5
+Di toko online, hitung
+total_belanjaan = "Total belanjaan Anda: "
+if
+total_belanjaan > 500000:
+diskon = total_belanjaan
+total_belanjaan >= 300000:
+elif
+diskon berdasarkan total belanjaan pelanggan
+* 0.10
+* 0.05
+diskon
+diskon
+= total_belanjaan
+total_pernbayaran = total_belanjaan - diskon
+print(f"Tota1 pembayaran setelah diskon: {total_pembayaran)" )
